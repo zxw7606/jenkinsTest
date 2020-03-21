1c411680a55596c4458a14102d86008b7a0f1fcb
zxw7606 <zxw7606@126.com>
2020-03-21 18:34:50 +0800

    first commit

---
+++ src/test/java/com/zxw/jenkinstest/JenkinstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxw.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
